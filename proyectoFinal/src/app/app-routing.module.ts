885950cd9fb967a9d3349c9a7c8a944503413f78
fabrimikele16 <69601751+fabrimikele16@users.noreply.github.com>
2020-09-01 22:58:39 -0400

    cambios

--- proyectoFinal/src/app/app-routing.module.ts
+++ proyectoFinal/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"registro",component:RegistroComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
